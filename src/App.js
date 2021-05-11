85d42be88429a1fd27816418c753404d0e86ad68
Marcos Douglas <marcosd.fernandes@uninove.edu.br>
2021-05-11 18:08:39 -0300

    Componentes stateful (state)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,48 @@
+import { Component } from 'react';
 import './App.css';
 import logo from './logo.svg';
 
-function App() {
+class App extends Component {
+  state = {
+    name: 'Marcos Douglas',
+    counter: 0,
+  };
+
+  handlePClick = () => {
+    this.setState({ name: 'Fernandes Atanazio' })
+  }
+
+  handleAClick = (event) => {
+    event.preventDefault()
+    const { counter } = this.state
+    this.setState({ counter: counter + 1 })
+  }
+
+  render() {
+    const { name, counter } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p onClick={this.handlePClick}>
+            {name} {counter}
+          </p>
+          <a
+            onClick={this.handleAClick}
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            This is Link
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+/* function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +61,6 @@ function App() {
       </header>
     </div>
   );
-}
+} */
 
 export default App;

e6e6dd9e2cc0c7e8243504d2f4c3ad3cc695b3b4
Marcos Douglas <marcosd.fernandes@uninove.edu.br>
2021-05-11 21:24:44 -0300

    Estado com arrays

--- src/App.js
+++ src/App.js
@@ -1,42 +1,37 @@
-import { Component } from 'react';
+import { Component, Fragment } from 'react';
 import './App.css';
-import logo from './logo.svg';
 
 class App extends Component {
   state = {
-    name: 'Marcos Douglas',
-    counter: 0,
+    posts: [
+      {
+        id: 1,
+        title: 'O título 1',
+        body: 'O corpo 1',
+      },
+      {
+        id: 2,
+        title: 'O título 2',
+        body: 'O corpo 2',
+      },
+      {
+        id: 3,
+        title: 'O título 3',
+        body: 'O corpo 3',
+      },
+    ]
   };
 
-  handlePClick = () => {
-    this.setState({ name: 'Fernandes Atanazio' })
-  }
-
-  handleAClick = (event) => {
-    event.preventDefault()
-    const { counter } = this.state
-    this.setState({ counter: counter + 1 })
-  }
-
   render() {
-    const { name, counter } = this.state;
+    const { posts } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p onClick={this.handlePClick}>
-            {name} {counter}
-          </p>
-          <a
-            onClick={this.handleAClick}
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            This is Link
-          </a>
-        </header>
+        {posts.map(post => (
+          <Fragment key={post.id}>
+            <h1>{post.title}</h1>
+            <h3>{post.body}</h3>
+          </Fragment>
+        ))}
       </div>
     );
   }

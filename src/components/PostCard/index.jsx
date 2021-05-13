db3f6b32dc6aecc50d3d5318e0951790c232c12c
unknown <Administrador@unifesp-168716.hsp.unifesp>
2021-05-13 17:26:49 -0300

    Props e organização - parte 3

--- src/components/PostCard/index.jsx
+++ src/components/PostCard/index.jsx
@@ -1,10 +1,12 @@
+import './styles.css'
+
 export const PostCard = ({ id, title, body, cover }) => {
   console.log()
   return (
     <div className="post">
       <img src={cover} alt={title} />
       <div className="post-content">
-        <h2>{title}</h2>
+        <h2>{title} {id}</h2>
         <p>{body}</p>
       </div>
     </div>

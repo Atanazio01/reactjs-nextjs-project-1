b5404f29f5e3eec18324ff789dfe56fc5559d8a5
unknown <Administrador@unifesp-168716.hsp.unifesp>
2021-05-13 12:27:18 -0300

    Props e organização - parte 2

--- src/App.js
+++ src/App.js
@@ -1,51 +1,26 @@
 import { Component } from 'react';
 import './App.css';
-import { PostCard } from './components/PostCard';
+import { Posts } from './components/Posts';
+import { loadPosts } from './utils/load-posts';
 
 class App extends Component {
   state = { // esse é o estado, quando atualiza os filhos, atualiza o estado
     posts: []
   };
-
   // utilizado na requisição de dados de API
-  componentDidMount() { // executa após o componente ser montado 
-    this.loadPosts();
+  async componentDidMount() { // executa após o componente ser montado 
+    await this.loadPosts();
   }
-
   loadPosts = async () => {
-    const postResponse = fetch('https://jsonplaceholder.typicode.com/posts');
-    const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos');
-
-    const [posts, photos] = await Promise.all([postResponse, photosResponse]);
-
-    const postsJson = await posts.json();
-    const photosJson = await photos.json();
-
-    const postsAndPhotos = postsJson.map((post, index) => {
-      return { ...post, cover: photosJson[index].url }
-    })
-
+    const postsAndPhotos = await loadPosts();
     this.setState({ posts: postsAndPhotos });
   }
-
   render() {
     const { posts } = this.state;
-
     return (
       <section className="container">
-        <div className="posts">
-          {posts.map(post => (
-            <PostCard
-              key={post.id}
-              id={post.id}
-              title={post.title}
-              body={post.body}
-              cover={post.cover} />
-            // post={post} também funciona, para o caso de muitos atbts
-          ))}
-        </div>
+        <Posts posts={posts} />
       </section>
-
     );
   }
 }

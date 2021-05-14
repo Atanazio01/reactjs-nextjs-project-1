05744550d3112fec3cb637fefc5e534f2c35fa41
Atanazio01 <Administrador@unifesp-168716.hsp.unifesp>
2021-05-14 16:22:39 -0300

    Trabalhando com inputs de texto

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 import { Button } from '../../components/Button';
 import { Posts } from '../../components/Posts';
+import { TextInput } from '../../components/TextInput';
 import { loadPosts } from '../../utils/load-posts';
 import './styles.css';
 
@@ -10,6 +11,7 @@ export class Home extends Component {
     allPosts: [],
     page: 0,
     postsPerPage: 5,
+    searchValue: '',
   };
 
   // utilizado na requisição de dados de API
@@ -40,20 +42,51 @@ export class Home extends Component {
     this.setState({ posts, page: nextPage })
   }
 
+  handleChange = (e) => {
+    const { value } = e.target;
+    this.setState({ searchValue: value })
+  }
+
   render() {
-    const { posts, page, postsPerPage, allPosts } = this.state;
+    const { posts, page, postsPerPage, allPosts, searchValue } = this.state;
     const noMorePosts = page + postsPerPage >= allPosts.length;
+
+    const filteredPosts = !!searchValue ? allPosts.filter(post => {
+      return post.title.toLowerCase().includes(
+        searchValue.toLowerCase()
+      )
+    })
+      : posts;
 
     return (
       <section className="container">
-        <Posts posts={posts} />
+        <div className='search-container'>
+          {!!searchValue && (
+            <h1>Search value: {searchValue}</h1>
+          )}
+
+          <TextInput
+            searchValue={searchValue}
+            handleChange={this.handleChange}
+          />
+        </div>
+
+        {filteredPosts.length > 0 && (
+          <Posts posts={filteredPosts} />
+        )}
+
+        {filteredPosts.length === 0 && (
+          <p>Não existem posts =(</p>
+        )}
 
         <div className="button-container">
-          <Button
-            text="Load more posts"
-            onClick={this.loadMorePosts}
-            disabled={noMorePosts}
-          />
+          {!searchValue && (
+            <Button
+              text="Load more posts"
+              onClick={this.loadMorePosts}
+              disabled={noMorePosts}
+            />
+          )}
         </div>
       </section>
     );
